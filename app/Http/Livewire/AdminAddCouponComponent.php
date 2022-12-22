3ca1dfdc439b9f3f26497c151c0069e0c0866f33
unknown <bayuxbca@gmail.com>
2022-12-22 14:54:10 +0700

    adding coupons component livewire

---
+++ app/Http/Livewire/AdminAddCouponComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class AdminAddCouponComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin-add-coupon-component');
+    }
+}
---
+++ app/Http/Livewire/AdminEditCouponComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class AdminEditCouponComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin-edit-coupon-component');
+    }
+}
